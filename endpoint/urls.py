a7b81e8abf6120bd70ca7c06a431584d44566645
Apiut Toel <toelapiut7@gmail.com>
2021-05-02 17:09:58 +0300

    refactor: renamed endpoints

--- endpoint/urls.py
+++ endpoint/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import QuandlAPIView
 
 urlpatterns = [
-    path('quandl/market', QuandlAPIView.as_view()),
-    path('quandl/stock/<str:ticker>', QuandlAPIView.as_view()),
+    path('quandl/markets', QuandlAPIView.as_view()),
+    path('quandl/stocks/<str:ticker>', QuandlAPIView.as_view()),
 ]
