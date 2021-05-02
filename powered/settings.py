555474dcff388f2e9f5ffca5be63e8ea466ed7c6
Apiut Toel <toelapiut7@gmail.com>
2021-05-02 19:22:18 +0300

    refactor: renamed endpoints

--- powered/settings.py
+++ powered/settings.py
@@ -56,6 +56,20 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:3000",
+    "https://127.0.0.1:3000",
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 ROOT_URLCONF = 'powered.urls'
 
 TEMPLATES = [
